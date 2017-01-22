b1d738e5b06940f98bb5c061988324249e300ef9
Valentin Silvestre <vasilvestre@laposte.net>
2017-01-22 12:47:20 +0100

    clean startup
    json get with volley

--- TechPlan/app/src/main/java/efficom/slam/groupe2/techplan/ListResponse.java
+++
@@ -1,8 +0,0 @@
-package efficom.slam.groupe2.techplan;
-
-/**
- * Created by lbettini on 20/01/17.
- */
-
-public class ListResponse {
-}
--- TechPlan/app/src/main/java/efficom/slam/groupe2/techplan/MainActivity.java
+++ TechPlan/app/src/main/java/efficom/slam/groupe2/techplan/MainActivity.java
@@ -1,95 +1,50 @@
 package efficom.slam.groupe2.techplan;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
 
-import java.io.IOException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
-    private class LongOperation extends AsyncTask<String, Integer, ListReponse> {
-        ListReponse listEtudiants = null;
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(ListReponse list) {
-            super.onPostExecute(list);
-            Toast.makeText(afficherListe.this, list.getEtudiants().toString(), Toast.LENGTH_LONG).show();
-            listViewEtudiants = (ListView) findViewById(R.id.listViewEtudiants);
-            ArrayAdapter<Etudiants> adapter = new ArrayAdapter<Etudiants>(afficherListe.this, android.R.layout.simple_list_item_1, list.getEtudiants());
-            listViewEtudiants.setAdapter(adapter);
-
-
-        }
-
-        @Override
-        protected ListReponse doInBackground(String... params) {
-            String s = null;
-            Gson gson = new Gson();
-            try {
-                s = run(params[0]);
-                listEtudiants = gson.fromJson(s, ListReponse.class);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-
-            return listEtudiants;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-        }
-    }
-
-
-    ListView listViewEtudiants;
+    private TextView tv_result;
+    private ListView lv_intervention;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_afficher_liste);
+        setContentView(R.layout.activity_main);
 
-        new LongOperation().execute("https://raw.githubusercontent.com/artandor/TPs-Android/master/tpAmine1901/etudiants.json");
-
-
+        getData();
     }
 
-    RequestQueue queue = Volley.newRequestQueue(this);
+    private void getData(){
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        String url = "https://raw.githubusercontent.com/artandor/TPs-Android/master/tpAmine1901/etudiants.json";
 
-    StringRequest stringRequest = new StringRequest(Request.Method.GET,url,
-            new Response.Listener<String>(){
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-              public void onResponse(String response){
-                  mtextView.setText("Response is :" + response.substring( 0,500));
-              }
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        tv_result.setText("Response: " + response.toString());
+                        //lv_intervention.setAdapter();
+                    }
+                }, new Response.ErrorListener() {
 
-            });
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
 
-    String run(String url) throws IOException {
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-
+                    }
+                });
+        Volley.newRequestQueue(this).add(jsObjRequest);
     }
 }
