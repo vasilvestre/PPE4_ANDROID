dea33b799d19b3bd83f96e26edae70edfd80bb0e
Valentin Silvestre <vasilvestre@laposte.net>
2017-02-25 23:47:21 +0100

    position manager imei add and put

--- TechPlan/app/src/main/java/efficom/slam/groupe2/techplan/PositionService.java
+++ TechPlan/app/src/main/java/efficom/slam/groupe2/techplan/PositionService.java
@@ -1,12 +1,23 @@
 package efficom.slam.groupe2.techplan;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -16,10 +27,13 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class PositionService extends Service {
-    public PositionService() {
-    }
+
+    public PositionService() {}
 
     private Timer timer = new Timer();
+
+    // TelephonyManager tm;
+    // String imei;
 
     @Override
     public void onCreate() {
@@ -29,13 +43,22 @@ public class PositionService extends Service {
             public void run() {
                 sendRequestPosition();
             }
-        }, 0, 1000 * 60 * 5);
+        }, 0, 1000 * 10);
     }
 
     private void sendRequestPosition() {
-        String url = "http://www.domy59efficom.eu/insUpdatePositionTechnicien";
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = tm.getDeviceId();
+        String url = "http://www.api.vsilvestre.fr/positions/"+imei;
 
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+
+
+        StringRequest postRequest = new StringRequest(Request.Method.PUT, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -52,13 +75,12 @@ public class PositionService extends Service {
             @Override
             public Map<String, String> getHeaders() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("imei", "123456789101118");
-                params.put("latitude", "Envoie android reussi");
-                params.put("longitude", "ez");
+                params.put("latitude", "50.50");
+                params.put("longitude", "40.40");
                 return params;
             }
         };
-        Volley.newRequestQueue(this).add(postRequest);
+        requestQueue.add(postRequest);
     }
 
     @Override
